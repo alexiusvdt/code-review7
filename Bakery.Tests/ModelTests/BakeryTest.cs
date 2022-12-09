f01b19fc76fa37a18b17388b454c437215a714a0
Alex Johnson <alex.johnson293@gmail.com>
2022-12-09 10:11:47 -0800

    finish MVP of project, begin additional feature work

--- Bakery.Tests/ModelTests/BakeryTest.cs
+++ Bakery.Tests/ModelTests/BakeryTest.cs
@@ -11,19 +11,19 @@ namespace Bakery.Tests
     public void BreadClass_CalculatesThirdDiscount_Bread()
     {
       int custBread = 15;
-      Assert.AreEqual(50, Bread.BreadTotal(15));
+      Assert.AreEqual(50, Bread.BreadTotal(custBread));
     }
     [TestMethod]
     public void BreadClass_CalculatesSecondDiscount_Bread()
     {
       int custBread = 5;
-      Assert.AreEqual(20, Bread.BreadTotal(5));
+      Assert.AreEqual(20, Bread.BreadTotal(custBread));
     }
     [TestMethod]
     public void BreadClass_CalculatesFirstDiscount_Bread()
     {
       int custBread = 4;
-      Assert.AreEqual(15, Bread.BreadTotal(4)); 
+      Assert.AreEqual(15, Bread.BreadTotal(custBread)); 
     }
   }
   
@@ -35,7 +35,12 @@ namespace Bakery.Tests
     public void PastryClass_CalculatesdDiscount_Pastry()
     {
       int custPastry = 6;
-      Assert.AreEqual(10, Pastry.PastryTotal(6));
+      Assert.AreEqual(10, Pastry.PastryTotal(custPastry));
+    }
+    public void PastryClass_DoubleCheckYourLogic_Pastry()
+    {
+      int custPastry = 300;
+      Assert.AreEqual(500, Pastry.PastryTotal(custPastry));
     }
 
   }
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -5,21 +5,21 @@ using Bakery.Models;
 namespace MyProgram {
   class Program {
     static void Main(){
-      Console.WriteLine("Bonjour! Today we sell bread at $5 (buy two, third is free!) and pastries starting at $2");
-      Console.WriteLine("=====================================================");
+      Console.WriteLine("Bonjour! Today we sell bread at $5 (buy two, third is free!) and pastries starting at $2 (every third is half off!)");
+      Console.WriteLine("===================================================================================================================");
       Console.WriteLine("How many loaves of bread would you like?");
       int custBread = int.Parse(Console.ReadLine());
       Console.WriteLine("How many pastries would you like?");
       int custPastry = int.Parse(Console.ReadLine());
-      // AssembleOrder()
-
-      
+      int custTotal = AssembleOrder(custBread, custPastry);
+      Console.WriteLine($"Your total today for {custBread} loaves and {custPastry} pastry is ${custTotal}, merci!"); 
+      Console.WriteLine();  
     }
 
-    // public static int AssembleOrder()
-    // {
-    //   //get bread cost
-    //   //get pastry cost
-    // }
+    public static int AssembleOrder(int custBread, int custPastry)
+    {
+      int orderTotal = (Bread.BreadTotal(custBread) + Pastry.PastryTotal(custPastry));
+      return orderTotal;
+    }
   }
 }
