a88bb82b10736a441bfa43e8876152336e227d69
Alex Johnson <alex.johnson293@gmail.com>
2022-12-09 09:55:21 -0800

    pass first pastry test

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -4,8 +4,18 @@ namespace Bakery.Models
 {
   public class Pastry
   {
-    int PastryCount{ get; set;}
+    public static int PastryCount { get; set;}
+    public static int CostPerPastry { get; set;}
+    public static int PastrySubtotal { get; set;}
     
-
+    public static int PastryTotal(int custPastry)
+    {
+      CostPerPastry = 2;
+      if (custPastry % 3 == 0)
+      {
+        PastrySubtotal = (custPastry * CostPerPastry) - ((custPastry / 3) * 1);
+      }
+    return PastrySubtotal;
+    }
   }
 }
